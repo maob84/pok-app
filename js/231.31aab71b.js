db9059388c20602031c6a552539499f1e990c288
Marcus Obst <marcus.obst@outlook.de>
2024-08-27 11:04:09 +0200

    Updates

---
+++ js/231.31aab71b.js
@@ -0,0 +1,304 @@
+"use strict";
+(globalThis["webpackChunkpok"] = globalThis["webpackChunkpok"] || []).push([[231],{
+
+/***/ 6166:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   $r: () => (/* binding */ toggleInstrumentMute),
+/* harmony export */   Bz: () => (/* binding */ turnTunerOn),
+/* harmony export */   Cl: () => (/* binding */ updateInstrumentPanorama),
+/* harmony export */   GY: () => (/* binding */ getRoomPresetCollections),
+/* harmony export */   LO: () => (/* binding */ selectInstrumentPreset),
+/* harmony export */   O8: () => (/* binding */ getCurrentRoomPresetCollection),
+/* harmony export */   Qy: () => (/* binding */ getInstrumentById),
+/* harmony export */   RV: () => (/* binding */ updateInstrumentVolume),
+/* harmony export */   U8: () => (/* binding */ selectCurrentRoomPresetCollection),
+/* harmony export */   Zr: () => (/* binding */ resetRoom),
+/* harmony export */   mw: () => (/* binding */ getControlQrImage),
+/* harmony export */   ok: () => (/* binding */ getWifiQrImage),
+/* harmony export */   pB: () => (/* binding */ getInstruments),
+/* harmony export */   uB: () => (/* binding */ getTuning),
+/* harmony export */   uJ: () => (/* binding */ getRoomStatus),
+/* harmony export */   xr: () => (/* binding */ turnTunerOff)
+/* harmony export */ });
+/* unused harmony exports apiClient, getTunerState */
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2303);
+
+
+//const apiUrl = process.env.VUE_APP_API_URL || 'http://192.168.1.9:8000';
+//const apiUrl = 'https://room.proben-ohne-krach.de:443';
+//const apiUrl = 'http://192.168.2.111:8000';
+
+const apiUrl = "http://192.168.0.102:8000";
+const apiClient = axios__WEBPACK_IMPORTED_MODULE_0__/* ["default"] */ .A.create({
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// General room services
+const getRoomPresetCollections = () => apiClient.get('/room/presetcollections');
+const getCurrentRoomPresetCollection = () => apiClient.get('/room/presetcollections/current');
+const selectCurrentRoomPresetCollection = id => apiClient.put('/room/presetcollections/current', null, {
+  params: {
+    id: id
+  }
+});
+const resetRoom = () => apiClient.post('/room/reset');
+const getWifiQrImage = () => apiClient.get('/room/wifi-qr-code', {
+  responseType: 'blob'
+});
+const getControlQrImage = () => apiClient.get('/room/control-qr-code', {
+  responseType: 'blob'
+});
+const getRoomStatus = () => apiClient.get('/room');
+
+// Instrument services
+const getInstruments = () => apiClient.get('/instruments');
+const getInstrumentById = id => apiClient.get(`/instruments/${id}`);
+const updateInstrumentVolume = (id, volume) => apiClient.patch(`/instruments/${id}/volume`, null, {
+  params: {
+    volume: volume
+  }
+});
+const updateInstrumentPanorama = (id, panorama) => apiClient.patch(`/instruments/${id}/panorama`, null, {
+  params: {
+    panorama: panorama
+  }
+});
+const selectInstrumentPreset = (id, preset_id) => apiClient.patch(`/instruments/${id}/preset`, null, {
+  params: {
+    preset_id: preset_id
+  }
+});
+const toggleInstrumentMute = id => apiClient.patch(`/instruments/${id}/mute`);
+
+// Tuner services
+const getTunerState = id => apiClient.get(`/instruments/${id}/tuner`);
+const turnTunerOn = id => apiClient.post(`/instruments/${id}/tuner/on`);
+const turnTunerOff = id => apiClient.post(`/instruments/${id}/tuner/off`);
+const getTuning = id => apiClient.get(`/instruments/${id}/tuning`);
+
+/***/ }),
+
+/***/ 6394:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Q: () => (/* binding */ useInstrumentStore)
+/* harmony export */ });
+/* harmony import */ var pinia__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1555);
+/* harmony import */ var _services_apiServices__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6166);
+/* harmony import */ var _boot_axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(660);
+
+
+
+const useInstrumentStore = (0,pinia__WEBPACK_IMPORTED_MODULE_2__/* .defineStore */ .nY)('instrumentStore', {
+  state: () => ({
+    isBackendAvailable: false,
+    selectedInstrumentId: null,
+    selectedInstrumentName: null,
+    selectedInstrumentType: null,
+    selectedPresetCollectionName: null,
+    isPreventScreenDimming: false,
+    roomStatus: null
+  }),
+  actions: {
+    togglePreventScreenDimming() {
+      this.isPreventScreenDimming = !this.isPreventScreenDimming;
+    },
+    setPreventScreenDimming(value) {
+      this.isPreventScreenDimming = value;
+    },
+    checkBackendAvailability() {
+      console.log('Checking backend availability...');
+      const response = _boot_axios__WEBPACK_IMPORTED_MODULE_1__.api.get('room').then(response => {
+        if (response.status === 200) {
+          this.isBackendAvailable = true;
+          this.roomStatus = response.data;
+          this.updateStore();
+        } else {
+          this.isBackendAvailable = false;
+        }
+      }).catch(error => {
+        console.log('Backend service is not available:', error);
+        this.isBackendAvailable = false;
+      });
+    },
+    setSelectedInstrument(id, name, type) {
+      this.selectedInstrumentId = id;
+      this.selectedInstrumentName = name;
+      this.selectedInstrumentType = type;
+    },
+    setPresetCollection(name) {
+      this.selectedPresetCollectionName = name;
+    },
+    updateStore() {
+      (0,_services_apiServices__WEBPACK_IMPORTED_MODULE_0__/* .getCurrentRoomPresetCollection */ .O8)().then(response => {
+        this.selectedPresetCollectionName = response.data.name;
+      }).catch(error => {
+        console.error('Error getting preset collection:', error);
+      });
+    }
+  },
+  getters: {
+    getSelectedPresetCollectionName: state => state.selectedPresetCollectionName,
+    getSelectedInstrumentId: state => state.selectedInstrumentId,
+    getSelectedInstrumentName: state => state.selectedInstrumentName,
+    getSelectedInstrumentType: state => state.selectedInstrumentType,
+    selectedInstrumentHasTuner: state => state.selectedInstrumentType === 'guitar'
+  }
+});
+
+/***/ }),
+
+/***/ 5231:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ InstrumentSelect)
+});
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(1758);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/InstrumentSelect.vue?vue&type=template&id=46fb37de&scoped=true
+
+const _withScopeId = n => (_pushScopeId("data-v-46fb37de"), n = n(), _popScopeId(), n);
+const _hoisted_1 = {
+  class: "column q-pa-md full-width"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_q_card_section = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-card-section");
+  const _component_q_btn = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-btn");
+  const _component_q_card = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-card");
+  const _component_q_page = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-page");
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createBlock */.Wv)(_component_q_page, {
+    class: "q-pa-md"
+  }, {
+    default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [(0,runtime_core_esm_bundler/* createVNode */.bF)(_component_q_card, {
+      flat: ""
+    }, {
+      default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [(0,runtime_core_esm_bundler/* createVNode */.bF)(_component_q_card_section, null, {
+        default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Wähle aus, welches Instrument du gerne über die App steuern möchtest. ")]),
+        _: 1
+      }), (0,runtime_core_esm_bundler/* createVNode */.bF)(_component_q_card_section, {
+        class: "flex flex-center"
+      }, {
+        default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", _hoisted_1, [((0,runtime_core_esm_bundler/* openBlock */.uX)(true), (0,runtime_core_esm_bundler/* createElementBlock */.CE)(runtime_core_esm_bundler/* Fragment */.FK, null, (0,runtime_core_esm_bundler/* renderList */.pI)($data.instruments, instrument => {
+          return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createBlock */.Wv)(_component_q_btn, {
+            padding: "sm lg",
+            rounded: "",
+            "no-caps": "",
+            color: "primary",
+            size: "lg",
+            class: "full-width q-mb-md",
+            label: instrument.name,
+            key: instrument.id,
+            onClick: $event => $options.selectInstrument(instrument.name, instrument.id, instrument.type)
+          }, null, 8, ["label", "onClick"]);
+        }), 128))])]),
+        _: 1
+      })]),
+      _: 1
+    })]),
+    _: 1
+  });
+}
+;// CONCATENATED MODULE: ./src/pages/InstrumentSelect.vue?vue&type=template&id=46fb37de&scoped=true
+
+// EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.push.js
+var es_array_push = __webpack_require__(239);
+// EXTERNAL MODULE: ./src/stores/instruments-store.js
+var instruments_store = __webpack_require__(6394);
+// EXTERNAL MODULE: ./src/services/apiServices.js
+var apiServices = __webpack_require__(6166);
+// EXTERNAL MODULE: ./node_modules/vue-router/dist/vue-router.mjs
+var vue_router = __webpack_require__(455);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/InstrumentSelect.vue?vue&type=script&lang=js
+
+
+
+
+/* harmony default export */ const InstrumentSelectvue_type_script_lang_js = ({
+  setup() {
+    const store = (0,instruments_store/* useInstrumentStore */.Q)();
+    const router = (0,vue_router/* useRouter */.rd)();
+    const setInstrument = (id, name, type) => {
+      store.setSelectedInstrument(id, name, type);
+    };
+    const goToInstrumentPage = () => {
+      router.push('/instrument');
+    };
+    return {
+      setInstrument,
+      goToInstrumentPage
+    };
+  },
+  data() {
+    return {
+      instruments: []
+    };
+  },
+  mounted() {
+    this.fetchInstruments();
+  },
+  methods: {
+    fetchInstruments() {
+      (0,apiServices/* getInstruments */.pB)().then(response => {
+        this.instruments = response.data;
+      }).catch(error => {
+        console.error('Error fetching instruments:', error);
+      });
+    },
+    selectInstrument(name, id, type) {
+      const store = (0,instruments_store/* useInstrumentStore */.Q)();
+      console.log(`Instrument selected! '${name}' '${id}' '${type}'`);
+      this.setInstrument(id, name, type);
+      this.goToInstrumentPage();
+    }
+  }
+});
+;// CONCATENATED MODULE: ./src/pages/InstrumentSelect.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(2807);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/page/QPage.js
+var QPage = __webpack_require__(7716);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/card/QCard.js
+var QCard = __webpack_require__(3316);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/card/QCardSection.js
+var QCardSection = __webpack_require__(4189);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/btn/QBtn.js
+var QBtn = __webpack_require__(6384);
+// EXTERNAL MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/runtime.auto-import.js
+var runtime_auto_import = __webpack_require__(8582);
+var runtime_auto_import_default = /*#__PURE__*/__webpack_require__.n(runtime_auto_import);
+;// CONCATENATED MODULE: ./src/pages/InstrumentSelect.vue
+
+
+
+
+;
+
+
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.A)(InstrumentSelectvue_type_script_lang_js, [['render',render],['__scopeId',"data-v-46fb37de"]])
+
+/* harmony default export */ const InstrumentSelect = (__exports__);
+;
+
+
+
+
+runtime_auto_import_default()(InstrumentSelectvue_type_script_lang_js, 'components', {QPage: QPage/* default */.A,QCard: QCard/* default */.A,QCardSection: QCardSection/* default */.A,QBtn: QBtn/* default */.A});
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=231.31aab71b.js.map
---
+++ js/45.7a8dfaae.js
@@ -0,0 +1,112 @@
+"use strict";
+(globalThis["webpackChunkpok"] = globalThis["webpackChunkpok"] || []).push([[45],{
+
+/***/ 9045:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ ImpressumPage)
+});
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(1758);
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/ImpressumPage.vue?vue&type=template&id=629531e8
+
+const _hoisted_1 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+  class: "q-mb-md text-h3"
+}, " Impressum ", -1);
+const _hoisted_2 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Angaben gemäß § 5 TMG")], -1);
+const _hoisted_3 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)("Cucumber Company - Proben ohne Krach"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Daniel Zimny, Sebastian Wunderlich & Marcus Obst GbR"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Bernsdorfer Str. 291"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" 09125 Chemnitz, FARADIT Gewerbepark")], -1);
+const _hoisted_4 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Vertreten durch:"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Dipl. Soz. Päd. (FH) Daniel Zimny"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Sebastian Wunderlich"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Dr.-Ing. Marcus Obst")], -1);
+const _hoisted_5 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Kontakt:"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" E-Mail: "), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("a", {
+  href: "mailto:info@proben-ohne-krach.de"
+}, "info@proben-ohne-krach.de")], -1);
+const _hoisted_6 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+  class: "text-h5"
+}, "Haftungsausschluss", -1);
+const _hoisted_7 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Haftung für Inhalte")], -1);
+const _hoisted_8 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.", -1);
+const _hoisted_9 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Haftung für Links")], -1);
+const _hoisted_10 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.", -1);
+const _hoisted_11 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Urheberrecht")], -1);
+const _hoisted_12 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers der Cucumber Company GbR. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.", -1);
+const _hoisted_13 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Datenschutz")], -1);
+const _hoisted_14 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.", -1);
+const _hoisted_15 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Nutzung des Kontaktformulars / Terminbuchung")], -1);
+const _hoisted_16 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit, mit uns über ein auf der Webseite bereitgestelltes Formulare Kontakt aufzunehmen. Dabei ist mindestens die Angabe einer gültigen E-Mail-Adresse und Ihr Name erforderlich, damit wir wissen, von wem die Anfrage stammt und um diese beantworten zu können. Weitere Angaben können freiwillig getätigt werden. Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns erfolgt gem. Art. 6 Abs. 1 S. 1 lit. a DSGVO auf Grundlage Ihrer freiwillig erteilten Einwilligung. Die für die Benutzung der Formulare von uns erhobenen personenbezogenen Daten werden nach Erledigung der von Ihnen gestellten Anfrage automatisch gelöscht.", -1);
+const _hoisted_17 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Rechtlicher Vorbehalt")], -1);
+const _hoisted_18 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, "Alle Rechte der Veröffentlichung, insbesondere Vervielfältigung, Nachdruck, Speicherung in elektronischen Medien und Veröffentlichung auf anderen Internet-Präsenzen, auch auszugsweise, bedürfen der ausdrücklichen Genehmigung des Domaininhabers von www.proben-ohne-krach.de . Auch die Verbreitung der Texte oder Grafiken auf jegliche Art, sei es elektronisch, mechanisch oder anderweitig, einschließlich durch Kopieren, Faxen, Aufnehmen, Umformulieren oder durch Nutzung eines Informations-, Speicher- oder Wiederbeschaffungssystems, ist ohne vorherige schriftliche Genehmigung durch die Cucumber Company GbR ausdrücklich untersagt.", -1);
+const _hoisted_19 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("strong", null, "Zuständige Behörde")], -1);
+const _hoisted_20 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("p", null, [/*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)("Finanzamt Chemnitz-Süd"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("br"), /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.eW)(" Steuer-Nr.: 214/577/00498")], -1);
+const _hoisted_21 = {
+  class: "q-mt-xl"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_q_btn = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-btn");
+  const _component_q_scroll_area = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-scroll-area");
+  const _component_q_page = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("q-page");
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createBlock */.Wv)(_component_q_page, {
+    class: "q-pa-md"
+  }, {
+    default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [(0,runtime_core_esm_bundler/* createVNode */.bF)(_component_q_scroll_area, {
+      style: {
+        "height": "85vh"
+      }
+    }, {
+      default: (0,runtime_core_esm_bundler/* withCtx */.k6)(() => [_hoisted_1, (0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", null, [_hoisted_2, _hoisted_3, _hoisted_4, _hoisted_5, _hoisted_6, _hoisted_7, _hoisted_8, _hoisted_9, _hoisted_10, _hoisted_11, _hoisted_12, _hoisted_13, _hoisted_14, _hoisted_15, _hoisted_16, _hoisted_17, _hoisted_18, _hoisted_19, _hoisted_20, (0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", _hoisted_21, [(0,runtime_core_esm_bundler/* createVNode */.bF)(_component_q_btn, {
+        "no-caps": "",
+        label: "Über die App",
+        to: "/debug",
+        color: "primary"
+      })])])]),
+      _: 1
+    })]),
+    _: 1
+  });
+}
+;// CONCATENATED MODULE: ./src/pages/ImpressumPage.vue?vue&type=template&id=629531e8
+
+;// CONCATENATED MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/loader.js.transform-quasar-imports.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-2.use[0]!./node_modules/@quasar/app-webpack/lib/webpack/loader.vue.auto-import-quasar.js??ruleSet[0].use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./src/pages/ImpressumPage.vue?vue&type=script&lang=js
+/* harmony default export */ const ImpressumPagevue_type_script_lang_js = ({
+  name: 'ImpressumPage',
+  data() {
+    return {};
+  }
+});
+;// CONCATENATED MODULE: ./src/pages/ImpressumPage.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(2807);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/page/QPage.js
+var QPage = __webpack_require__(7716);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/scroll-area/QScrollArea.js
+var QScrollArea = __webpack_require__(9383);
+// EXTERNAL MODULE: ./node_modules/quasar/src/components/btn/QBtn.js
+var QBtn = __webpack_require__(6384);
+// EXTERNAL MODULE: ./node_modules/@quasar/app-webpack/lib/webpack/runtime.auto-import.js
+var runtime_auto_import = __webpack_require__(8582);
+var runtime_auto_import_default = /*#__PURE__*/__webpack_require__.n(runtime_auto_import);
+;// CONCATENATED MODULE: ./src/pages/ImpressumPage.vue
+
+
+
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.A)(ImpressumPagevue_type_script_lang_js, [['render',render]])
+
+/* harmony default export */ const ImpressumPage = (__exports__);
+;
+
+
+
+runtime_auto_import_default()(ImpressumPagevue_type_script_lang_js, 'components', {QPage: QPage/* default */.A,QScrollArea: QScrollArea/* default */.A,QBtn: QBtn/* default */.A});
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=45.7a8dfaae.js.map
